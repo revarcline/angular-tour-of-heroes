f49797a2db3ad4cc5769d2645b77f6aa4f4a2742
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-08 18:56:53 -0500

    using observable service stuff

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeroService {
+  constructor() {}
 
-  constructor() { }
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
+  }
 }
