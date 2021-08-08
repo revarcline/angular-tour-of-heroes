eedf240de461bf0b3506852eab5699538c95181f
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-08 14:09:24 -0500

    now using mock data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from "./app.component";
-import { HeroesComponent } from "./heroes/heroes.component";
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
-import { FormsModule } from "@angular/forms";
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,16 +1,14 @@
-import { Component, OnInit } from "@angular/core";
-import { Hero } from "../hero";
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
-  selector: "app-heroes",
-  templateUrl: "./heroes.component.html",
-  styleUrls: ["./heroes.component.css"],
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: "Windstorm",
-  };
+  heroes = HEROES;
 
   constructor() {}
 
