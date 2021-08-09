a15bac02ef5427cc4adb5c4f43bfea1c5001f125
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-09 17:03:32 -0500

    baleet

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -49,6 +49,14 @@ export class HeroService {
     );
   }
 
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap((_) => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send error to remote logging infrastructure
