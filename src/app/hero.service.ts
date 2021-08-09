233395d0dc4770f46449417609ecd9cc9b00b2a6
Alex Cline <rev.a.r.cline@gmail.com>
2021-08-09 17:09:49 -0500

    hero search

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -57,6 +57,21 @@ export class HeroService {
     );
   }
 
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap((x) =>
+        x.length
+          ? this.log(`found heroes matching "${term}"`)
+          : this.log(`no heroes matching "${term}"`)
+      ),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
       // TODO: send error to remote logging infrastructure
